4413f498016c847dc2e9488149aba8ccf41de379
Alexander Podlesnyi <gwebdzen@gmail.com>
2019-12-21 03:10:03 +0200

    Updates

---
+++ precache-manifest.5b54041f256c151b836baccfc9babe32.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8eb441e999c1a8939bcb5190bf7e7e",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "3e54e10befb3f0b67f4f",
+    "url": "/star-db/static/css/main.81b28b39.chunk.css"
+  },
+  {
+    "revision": "d7fe4ac9d08a7095b23e",
+    "url": "/star-db/static/js/2.3e9cedc5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/star-db/static/js/2.3e9cedc5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e54e10befb3f0b67f4f",
+    "url": "/star-db/static/js/main.0896342f.chunk.js"
+  },
+  {
+    "revision": "8ea794f168a39c5c1a29",
+    "url": "/star-db/static/js/runtime-main.fca3eed0.js"
+  },
+  {
+    "revision": "54567dbf1be3f674a99802fcc34b892b",
+    "url": "/star-db/static/media/StarWars.54567dbf.png"
+  },
+  {
+    "revision": "0bb77596d760f3ef0ab9a8d326d1d500",
+    "url": "/star-db/static/media/colach.0bb77596.jpg"
+  },
+  {
+    "revision": "74d7a07e2f02bce3e111ad8d2808f116",
+    "url": "/star-db/static/media/star.74d7a07e.jpg"
+  }
+]);
