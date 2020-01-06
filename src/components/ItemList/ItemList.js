e52dbc7b2d3c0dbf284ea838deb2ff170ed82a71
Alexander Podlesnyi <gwebdzen@gmail.com>
2020-01-06 17:41:16 +0200

    Refactoring ItemList

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,74 +1,33 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import Swapi from '../../services/swapi';
-import ErrorIndicator from '../ErrorIndicator';
-import Spinner from '../Spinner';
-
 import './ItemList.css';
 
-class ItemList extends Component {
-  static propTypes = {
-    onItemSelect: PropTypes.func.isRequired,
-  };
+const ItemList = props => {
+  const {
+    data: { itemList },
+    onItemSelect,
+    renderItem,
+  } = props;
 
-  state = {
-    people: null,
-    error: false,
-  };
+  const items = itemList.map(items => {
+    const { id } = items;
+    const item = renderItem(items);
 
-  swapi = new Swapi();
-
-  componentDidMount() {
-    this.peopleList();
-  }
-
-  errorSet = () => {
-    this.setState({
-      error: true,
-    });
-  };
-
-  async peopleList() {
-    try {
-      const people = await this.swapi.getAllPeoples();
-
-      this.setState({
-        people,
-      });
-    } catch (e) {
-      this.errorSet();
-    }
-  }
-
-  renderItem(people) {
-    const { onItemSelect } = this.props;
-
-    return people.map(({ id, name }) => {
-      return (
-        <li
-          className="list-group-item"
-          key={id}
-          onClick={() => onItemSelect(id)}
-          role="presentation"
-        >
-          {name}
-        </li>
-      );
-    });
-  }
-
-  render() {
-    const { people, error } = this.state;
-    if (!people) return <Spinner />;
-
-    const items = this.renderItem(people);
     return (
-      <ul className="item-list list-group">
-        {(!error && items) || <ErrorIndicator />}
-      </ul>
+      <li
+        className="list-group-item"
+        key={id}
+        onClick={() => onItemSelect(id)}
+        role="presentation"
+      >
+        {item}
+      </li>
     );
-  }
-}
+  });
+
+  return <ul className="item-list list-group">{items}</ul>;
+};
 
 export default ItemList;
